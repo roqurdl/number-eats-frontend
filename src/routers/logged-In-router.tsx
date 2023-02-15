a82405dae031cc4849e4263ce8a5288876796a20
roqurdl <uyhnri@gmail.com>
2023-02-15 16:48:03 +0900

    login practice

--- src/routers/logged-In-router.tsx
+++ src/routers/logged-In-router.tsx
@@ -1,3 +1,10 @@
 import React from "react";
 
-export const LoggedInRouter = () => <span>Logged In</span>;
+import { isLoggedInVar } from "../apollo";
+
+export const LoggedInRouter = () => (
+  <div>
+    <h1>Logged In</h1>
+    <button onClick={() => isLoggedInVar(false)}>Log Out</button>
+  </div>
+);
