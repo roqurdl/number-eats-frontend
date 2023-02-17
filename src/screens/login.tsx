cc619a45ebdbdd5b631fbd6a88d0362768933e2f
roqurdl <uyhnri@gmail.com>
2023-02-17 19:41:31 +0900

    login setup

--- src/screens/login.tsx
+++ src/screens/login.tsx
@@ -1,22 +1,72 @@
+import { gql, useMutation } from "@apollo/client";
 import React from "react";
+import { useForm } from "react-hook-form";
+import { FormError } from "../components/form-error";
+
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(input: { email: $email, password: $password }) {
+      ok
+      token
+      error
+    }
+  }
+`;
+
+interface loginForm {
+  email?: string;
+  password?: string;
+}
 
 export const Login = () => {
+  const {
+    register,
+    getValues,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<loginForm>();
+  const [loginMutation] = useMutation(LOGIN_MUTATION);
+  const onSubmit = () => {
+    const { email, password } = getValues();
+    loginMutation({
+      variables: {
+        email,
+        password: 1212121112,
+      },
+    });
+  };
   return (
     <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg py-10 rounded-lg text-center">
+      <div className="bg-white w-full max-w-lg pt-10 pb-7 rounded-lg text-center">
         <h3 className="text-2xl text-gray-800">Log In</h3>
-        <form className="flex flex-col mt-5 px-5">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col mt-5 px-5">
           <input
+            {...register(`email`, { required: "Email is required" })}
+            type="email"
             placeholder="Email"
-            className=" bg-gray-100 shadow-inner   border-2 focus:border-opacity-60 focus:border-green-600 focus:outline-none mb-3 py-3 px-5 rounded-lg"
+            className=" mb-3 input"
           />
+          {errors.email?.message && (
+            <FormError errorMessage={errors.email?.message} />
+          )}
           <input
+            {...register(`password`, {
+              required: "Password is required",
+              minLength: 8,
+            })}
+            type="password"
             placeholder="Password"
-            className=" bg-gray-100 shadow-inner focus:outline-none border-2 focus:border-opacity-60 focus:border-green-600  py-3 px-5 rounded-lg"
+            className="input"
           />
-          <button className="py-3 px-5 bg-gray-800 text-white mt-3 text-lg rounded-lg focus:outline-none hover:opacity-90">
-            Log In
-          </button>
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message} />
+          )}
+          {errors.password?.type === "minLength" && (
+            <FormError errorMessage="Password must be more than 8 chars." />
+          )}
+          <button className="mt-3 btn">Log In</button>
         </form>
       </div>
     </div>
